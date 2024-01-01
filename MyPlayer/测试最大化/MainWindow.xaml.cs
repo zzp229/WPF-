2f9686abab0e422f9306707f8998477ea83890b2
“nongnong” <a13556632276@gmail.com>
2024-01-02 01:02:02 +0800

    实现F键的视频最大化，MVVM的分离；测试最大化由备胎转正

--- MyPlayer/测试最大化/MainWindow.xaml.cs
+++ MyPlayer/测试最大化/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using 测试最大化.ViewModels;
 
 namespace 测试最大化
 {
@@ -20,19 +22,77 @@ namespace 测试最大化
     /// </summary>
     public partial class MainWindow : Window
     {
+        MainWindowViewModel viewModel;
         public MainWindow()
         {
             InitializeComponent();
+
+            viewModel = new MainWindowViewModel();
+            this.DataContext = viewModel;
+
+            this.KeyDown += Window_KeyDown;
+            
         }
+
+
+        // 这个快捷键好像没有办法抽离到ViewModel
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F)
+            {
+                if (this.WindowState != System.Windows.WindowState.Maximized)
+                {
+                    // 隐藏其他 UI 元素
+                    viewModel.BtnVisiable = Visibility.Collapsed;   // 隐藏
+
+                    // 修改 Grid 的行定义，以便 MediaElement 可以占据整个 Grid
+                    Grid.SetRowSpan(myBorder, 3);   
+                    this.MainGrid.RowDefinitions[0].Height = new GridLength(0);
+                    this.MainGrid.RowDefinitions[2].Height = new GridLength(0);
+
+                    // 设置窗口状态为最大化
+                    this.WindowState = System.Windows.WindowState.Maximized;
+                }
+                else
+                {
+                    // 恢复其他 UI 元素的可见性
+                    viewModel.BtnVisiable = Visibility.Visible;
+
+                    // 恢复 Grid 的行定义
+                    Grid.SetRowSpan(myBorder, 1);
+                    this.MainGrid.RowDefinitions[0].Height = new GridLength(1, GridUnitType.Star);
+                    this.MainGrid.RowDefinitions[1].Height = new GridLength(9, GridUnitType.Star);
+                    this.MainGrid.RowDefinitions[2].Height = new GridLength(1, GridUnitType.Star);
+
+                    // 恢复窗口状态
+                    this.WindowState = System.Windows.WindowState.Normal;
+                }
+            }
+        }
+
+
+
 
         private void a_Click(object sender, RoutedEventArgs e)
         {
             this.WindowState = System.Windows.WindowState.Maximized;
         }
 
+
         private void b_Click(object sender, RoutedEventArgs e)
         {
             this.WindowState = System.Windows.WindowState.Normal;
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            if (((DataObject)e.Data).GetFileDropList()[0] is string filename)
+            {
+                myMediaElement.Source = new Uri(filename, UriKind.Relative);
+                myMediaElement.LoadedBehavior = MediaState.Manual;
+                myMediaElement.UnloadedBehavior = MediaState.Manual;
+                myMediaElement.Play();
+            }
         }
     }
 }
---
+++ MyPlayer/测试最大化/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using 测试最大化.ViewModels.Helper;
+
+namespace 测试最大化.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+        public MainWindowViewModel()
+        {
+            closeCommand = new RelayCommand(ExecuteCloseCommand, CanExecuteCloseCommand);
+        }
+
+
+
+        #region CommandMethod
+        private bool CanExecuteCloseCommand(object obj)
+        {
+            return true;
+        }
+        private void ExecuteCloseCommand(object obj)
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+
+
+
+        #region Property
+        private Visibility btnVisiable;
+        /// <summary>
+        /// 控制按钮是否显示
+        /// </summary>
+        public Visibility BtnVisiable
+        {
+            get { return btnVisiable; }
+            set
+            {
+                btnVisiable = value;
+                OnPropertyChanged(nameof(BtnVisiable));
+            }
+        }
+        #endregion
+
+        #region Command
+        public ICommand closeCommand {  get; }
+        #endregion
+
+
+
+
+        // 通知属性要实现的
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
